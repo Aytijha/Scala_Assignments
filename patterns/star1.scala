5ef35886c3fbef76b19e28fdae1ada481d7838a6
Aytijha Chakraborty <ac.aytijha@gmail.com>
2021-10-26 12:39:19 +0530

    Create star1.scala

---
+++ patterns/star1.scala
@@ -0,0 +1,38 @@
+// without Array
+/*object HelloWorld {
+	def main(args: Array[String]): Unit = {
+	  var n = scala.io.StdIn.readInt()
+  	for(i<-0 until n){
+  	  for(j<- 0 until n){
+  	    if(i>=j){
+  	      print('*')
+  	    }
+  	  }
+  	  print('\n')
+  	}
+	}
+}*/
+
+// with Array
+
+import Array.ofDim
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+	  var n = scala.io.StdIn.readInt()
+	  var arr = ofDim[String](4,4)
+  	for(i<-0 until n){
+  	  for(j<- 0 until n){
+  	    if(i>=j){
+  	      arr(i)(j) = "*"
+  	    }
+  	    else arr(i)(j) = " "
+  	  }
+  	}
+  	for(i<-0 until n){
+  	  for(j<- 0 until n){
+  	    print(arr(i)(j))
+  	  }
+  	  print('\n')
+  	}
+	}
+}
