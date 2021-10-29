135b881b5f29298ed524679198ba9f6ffeb5ce3d
Aytijha Chakraborty <ac.aytijha@gmail.com>
2021-10-29 11:38:49 +0530

    Create Initialize.scala

---
+++ Lists/Initialize.scala
@@ -0,0 +1,8 @@
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+  	var a:List[String] = List("Aytijha", "Naruto", "Jiraiya", "Itachi", "Pain")
+  	var b:List[Int] = 1::(2::(3::(4::Nil)))
+  	println(a)
+  	println(b)
+	}
+}
