6b19e8c0803a57692154d1d5533b259abeb75087
Aytijha Chakraborty <ac.aytijha@gmail.com>
2021-11-18 21:05:10 +0530

    Practical-01 added

---
+++ Practical-01/HelloWorld.scala
@@ -0,0 +1,15 @@
+/*
+Name: AYTIJHA CHAKRABORTY
+BTech (CSE-AI/ML)   Sem: 5
+Class Roll: 17      Sec: K
+University Roll: 1914009
+
+Problem Statement:
+Write a Scala Program to print any sample message over console: Example: “Hello World!”
+*/
+
+object HelloWorld{
+    def main(args: Array[String]){
+        println("Hello World!")
+    }
+}
